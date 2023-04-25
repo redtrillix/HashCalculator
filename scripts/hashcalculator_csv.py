cf30b5d2993294eaca975578b9b428b35fc0be6c
Zachary G <zachary.gethin@destronia.com>
2023-04-26 02:30:06 +0800

    added date/time for csv
    * added a date and time column for the csv file

--- scripts/hashcalculator_csv.py
+++ scripts/hashcalculator_csv.py
@@ -1,12 +1,13 @@
-# A Hash Calculator script written in python for a comma seperated value (csv) file output
+# A Hash Calculator script written in python for a comma separated value (csv) file output
 # Made by redtrillix
-# Version: v0.2.2
+# Version: v0.3.0
 
 import hashlib
 import os
 import csv
 import tkinter as tk
 from tkinter import filedialog
+from datetime import datetime
 
 # Select files using file explorer
 root = tk.Tk()
@@ -21,11 +22,14 @@ for file_path in files:
     hashes[file_path] = file_hash
 
 # Write hashes to a CSV file
-csv_path = "hashes.csv"
+output_dir = "data"
+os.makedirs(output_dir, exist_ok=True)
+csv_path = os.path.join(output_dir, "hashes.csv")
 file_exists = os.path.isfile(csv_path)
 with open(csv_path, "a", newline="") as f:
     writer = csv.writer(f)
     if not file_exists:
-        writer.writerow(["File Path", "Hash Value"])
+        writer.writerow(["File Path", "Hash Value", "Date"])
+    calculated_date = datetime.now().strftime("%Y/%m/%d %H:%M:%S")
     for file_path, file_hash in hashes.items():
-        writer.writerow([file_path, file_hash])
+        writer.writerow([file_path, file_hash, calculated_date])
